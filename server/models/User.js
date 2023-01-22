6f4cc0794d17efe22a1cbaf268b10cafb22b9e2a
Pogtato <erikdeltaco@gmail.com>
2023-01-22 08:05:26 +0100

    first commit

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 50,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+      min: 5,
+      max: 50,
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+const User = model("User", UserSchema);
+export default User;
