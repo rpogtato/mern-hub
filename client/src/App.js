710861f3882b8532486dbe9581b49a4500649fdc
Pogtato <erikdeltaco@gmail.com>
2023-01-22 22:46:04 +0100

    ayudame

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "components/homePage";
-import LoginPage from "components/loginPage";
-import ProfilePage from "components/profilePage";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
 
 function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/profile/:userId" element={<ProfilePage />} />
-    </Routes>
-  </BrowserRouter>;
-
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
