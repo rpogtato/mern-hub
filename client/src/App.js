79c37b7bac906fc55f140300cd7f40dc7df3b747
Pogtato <erikdeltaco@gmail.com>
2023-01-22 20:18:03 +0100

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "components/homePage";
+import LoginPage from "components/loginPage";
+import ProfilePage from "components/profilePage";
 
 function App() {
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/profile/:userId" element={<ProfilePage />} />
+    </Routes>
+  </BrowserRouter>;
+
   return <div className="app"></div>;
 }
 
