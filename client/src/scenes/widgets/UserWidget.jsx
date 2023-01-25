74ade0217904f58ec4f170736322561e405347fe
Pogtato <erikdeltaco@gmail.com>
2023-01-25 05:34:47 +0100

    m

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -5,14 +5,14 @@ import {
   WorkOutlineOutlined,
 } from "@mui/icons-material";
 import { Box, Typography, Divider, useTheme } from "@mui/material";
-import UserImage from "components/Userimage";
+import UserImage from "components/UserImage";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-export function UserWidget({ userId, picturePath }) {
+export default function UserWidget({ userId, picturePath }) {
   const [user, setUser] = useState(null);
   const { palette } = useTheme();
   const navigate = useNavigate();
